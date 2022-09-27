a852e1b54e22bcf17787952f94e127bd53fb7db5
chrisimata21 <chrisimata21@gmail.com>
2022-09-27 11:13:06 -0700

    added license badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-const licenseArr = [];
-function renderLicenseBadge(license) {}
+const licenseArr = [
+  "GNU AGPLv3",
+  "GNU GPLv3",
+  "GNU LGPLv3",
+  "Mozilla Public License 2.0",
+  "Apache License 2.0",
+  "MIT License",
+  "Boost Software License 1.0",
+  "The Unlicense",
+];
+function renderLicenseBadge(license) {
+  if (license === licenseArr[0]) {
+    return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+  } else if (license === licenseArr[1]) {
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  } else if (license === licenseArr[2]) {
+    return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+  } else if (license === licenseArr[3]) {
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+  } else if (license === licenseArr[4]) {
+    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else if (license === licenseArr[5]) {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === licenseArr[6]) {
+    return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+  } else if (license === licenseArr[7]) {
+    return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
